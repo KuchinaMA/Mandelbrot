587fd710789aeaa86a71d560f73cef17151b7da9
KuchinaMA <m.kuchina05@mail.ru>
2024-03-31 23:45:50 +0300

    Separated into functions

--- Version3.cpp
+++ Version3.cpp
@@ -3,6 +3,14 @@
 
 #define TIME_MEASUREMENT
 
+static const float window_width  = 800.f;
+static const float window_height = 600.f;
+
+static const float dx = 1/window_width;
+static const float dy = 1/window_width;
+
+typedef RGBQUAD (&scr_t)[(unsigned long long)window_height][(unsigned long long)window_width];
+
 static const float x_centre = -1.325f;
 static const float y_centre = 0;
 
@@ -17,7 +25,9 @@ volatile int N_iter = 0;
 
 extern "C" uint64_t get_time();
 
-void view_regulation (float* x_shift, float* y_shift, float dx, float dy);
+void view_regulation (float* x_shift, float* y_shift);
+void draw_pixels(int* N_iterations, RGBQUAD scr[(size_t)window_height][(size_t)window_width], size_t ix, size_t iy);
+void count_mandelbrot(float x_shift, float y_shift, RGBQUAD scr[(size_t)window_height][(size_t)window_width], float* multipliers, float* R2_max);
 
 inline void mm256_set_ps (float res[points_num], float val7, float val6, float val5, float val4, float val3, float val2, float val1, float val0);
 inline void mm256_set1_ps (float res[points_num], float val);
@@ -39,12 +49,6 @@ int main() {
 
     Win32::_fpreset();
 
-    static const float width  = 800.f;
-    static const float height = 600.f;
-
-    const float dx = 1/width;
-    const float dy = 1/width;
-
     float x_shift = 0.f;
     float y_shift = 0.f;
 
@@ -56,18 +60,19 @@ int main() {
 
     #ifndef TIME_MEASUREMENT
 
-    txCreateWindow(width, height);
+    txCreateWindow(window_width, window_height);
     txBegin();
+    #endif
 
-    typedef RGBQUAD (&scr_t)[(unsigned long long)height][(unsigned long long)width];
     scr_t scr = (scr_t) *txVideoMemory();
 
+    #ifndef TIME_MEASUREMENT
     for (;;) {
 
         if (txGetAsyncKeyState (VK_ESCAPE))
             break;
 
-        view_regulation(&x_shift, &y_shift, dx, dy);
+        view_regulation(&x_shift, &y_shift);
 
         #else
 
@@ -77,88 +82,8 @@ int main() {
             uint64_t start_time = get_time();
             #endif
 
-            for (size_t iy = 0; iy < (size_t)height; iy++) {
+            count_mandelbrot(x_shift, y_shift, scr, multipliers, R2_max);
 
-                if (txGetAsyncKeyState (VK_ESCAPE))
-                    break;
-
-                float x0 = ((          -  (width / 2)) * dx + x_centre + x_shift);
-                float y0 = (((float)iy - (height / 2)) * dy + y_centre + y_shift);
-
-                for (size_t ix = 0; ix < (size_t)width; ix += points_num, x0 += points_num*dx) {
-
-                    float DX[points_num] = {};
-                    mm256_set1_ps(DX, dx);
-
-                    mm256_mul_ps(DX, DX, multipliers);
-
-                    float X0[points_num] = {};
-                    mm256_set1_ps(X0, x0);
-
-                    mm256_add_ps(X0, X0, DX);
-
-                    float X[points_num] = {};
-                    mm256_cpy_ps(X, X0);
-
-
-                    float Y0[points_num] = {};
-                    mm256_set1_ps(Y0, y0);
-
-                    float Y[points_num] = {};
-                    mm256_cpy_ps(Y, Y0);
-
-
-                    int N_iterations[points_num] = {};
-                    mm256_setzero_si256(N_iterations);
-
-                    for (size_t n_iter = 0; n_iter < N_iterations_max; n_iter++) {
-
-                        float X2[points_num] = {};
-                        mm256_mul_ps(X2, X, X);
-
-                        float Y2[points_num] = {};
-                        mm256_mul_ps(Y2, Y, Y);
-
-                        float XY[points_num] = {};
-                        mm256_mul_ps(XY, X, Y);
-
-
-                        float R2[points_num] = {};
-                        mm256_add_ps(R2, X2, Y2);
-
-                        float cmp[points_num] = {};
-                        mm256_cmple_ps(cmp, R2, R2_max);
-
-                        int mask = mm256_movemask_ps(cmp);
-                        if (!mask)
-                            break;
-
-                        mm256_sub_epi32(N_iterations, N_iterations, cmp);
-
-                        mm256_sub_ps(X, X2, Y2);
-                        mm256_add_ps(X, X, X0);
-
-                        mm256_add_ps(Y, XY, XY);
-                        mm256_add_ps(Y, Y, Y0);
-
-                    }
-                    N_iter = N_iterations[0];
-
-                    #ifndef TIME_MEASUREMENT
-
-                    for (size_t i = 0; i < points_num; i++) {
-
-                        float I = sqrtf (sqrtf ((float)N_iterations[i] / (float)N_iterations_max)) * 255.f;
-
-                        BYTE c = (BYTE)I;
-
-                        RGBQUAD color = (N_iterations[i] < N_iterations_max) ? RGBQUAD {(BYTE) (255 - c), (BYTE) (c%2 * 64), c} : RGBQUAD {};
-                        scr[iy][ix+i] = color;
-                    }
-                    #endif
-                }
-
-            }
             #ifdef TIME_MEASUREMENT
             uint64_t time_taken = get_time() - start_time;
             general_time += time_taken;
@@ -166,15 +91,14 @@ int main() {
 
         printf("%llu\n", general_time/running_num);
         #else
-
-        printf("FPS: \t\r%.0lf", txGetFPS());
+        printf("\t\t\rFPS: %.0lf", txGetFPS());
         txSleep();
     }
     #endif
 
 }
 
-void view_regulation (float* x_shift, float* y_shift, float dx, float dy) {
+void view_regulation (float* x_shift, float* y_shift) {
 
     if (txGetAsyncKeyState (VK_RIGHT))
         *x_shift += dx * (txGetAsyncKeyState (VK_SHIFT) ? 100.f : 10.f);
@@ -189,6 +113,97 @@ void view_regulation (float* x_shift, float* y_shift, float dx, float dy) {
         *y_shift -= dy * (txGetAsyncKeyState (VK_SHIFT) ? 100.f : 10.f);
 
 
+}
+
+void draw_pixels(int* N_iterations, RGBQUAD scr[(size_t)window_height][(size_t)window_width], size_t ix, size_t iy) {
+
+    for (size_t i = 0; i < points_num; i++) {
+
+        float I = sqrtf (sqrtf ((float)N_iterations[i] / (float)N_iterations_max)) * 255.f;
+
+        BYTE c = (BYTE)I;
+
+        RGBQUAD color = (N_iterations[i] < N_iterations_max) ? RGBQUAD {(BYTE) (255 - c), (BYTE) (c%2 * 64), c} : RGBQUAD {};
+        scr[iy][ix+i] = color;
+    }
+
+}
+
+void count_mandelbrot(float x_shift, float y_shift, RGBQUAD scr[(size_t)window_height][(size_t)window_width], float* multipliers, float* R2_max) {
+
+
+    for (size_t iy = 0; iy < (size_t)window_height; iy++) {
+
+        if (txGetAsyncKeyState (VK_ESCAPE))
+            break;
+
+        float x0 = ((          -  (window_width / 2)) * dx + x_centre + x_shift);
+        float y0 = (((float)iy - (window_height / 2)) * dy + y_centre + y_shift);
+
+        for (size_t ix = 0; ix < (size_t)window_width; ix += points_num, x0 += points_num*dx) {
+
+            float DX[points_num] = {};
+            mm256_set1_ps(DX, dx);
+
+            mm256_mul_ps(DX, DX, multipliers);
+
+            float X0[points_num] = {};
+            mm256_set1_ps(X0, x0);
+
+            mm256_add_ps(X0, X0, DX);
+
+            float X[points_num] = {};
+            mm256_cpy_ps(X, X0);
+
+
+            float Y0[points_num] = {};
+            mm256_set1_ps(Y0, y0);
+
+            float Y[points_num] = {};
+            mm256_cpy_ps(Y, Y0);
+
+
+            int N_iterations[points_num] = {};
+            mm256_setzero_si256(N_iterations);
+
+            for (size_t n_iter = 0; n_iter < N_iterations_max; n_iter++) {
+
+                float X2[points_num] = {};
+                mm256_mul_ps(X2, X, X);
+
+                float Y2[points_num] = {};
+                mm256_mul_ps(Y2, Y, Y);
+
+                float XY[points_num] = {};
+                mm256_mul_ps(XY, X, Y);
+
+
+                float R2[points_num] = {};
+                mm256_add_ps(R2, X2, Y2);
+
+                float cmp[points_num] = {};
+                mm256_cmple_ps(cmp, R2, R2_max);
+
+                int mask = mm256_movemask_ps(cmp);
+                if (!mask)
+                    break;
+
+                mm256_sub_epi32(N_iterations, N_iterations, cmp);
+
+                mm256_sub_ps(X, X2, Y2);
+                mm256_add_ps(X, X, X0);
+
+                mm256_add_ps(Y, XY, XY);
+                mm256_add_ps(Y, Y, Y0);
+
+            }
+            N_iter = N_iterations[0];
+
+            #ifndef TIME_MEASUREMENT
+            draw_pixels(N_iterations, scr, ix, iy);
+            #endif
+        }
+    }
 }
 
 
